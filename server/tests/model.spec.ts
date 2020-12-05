3958092ee32fee35061e3bdda4539b3e0edd6a6e
yovchenko <volodymyr.yovchenko@gmail.com>
2020-12-05 18:32:49 +0200

    test(model): create the app model unit tests

--- server/tests/model.spec.ts
+++ server/tests/model.spec.ts
@@ -1,19 +1,33 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import Model, { sqlQuery } from "../source/model";
 
-// eslint-disable-next-line @typescript-eslint/camelcase
-const output: [{ folder_id: string }] = [{ folder_id: undefined }];
+const root: [{ folder_id: string | null }] = [{ folder_id: null }];
+const workTypes: Array<{ erp_work_type: string }> = [];
 
 beforeAll(async () => {
-  // eslint-disable-next-line @typescript-eslint/camelcase
-  output[0].folder_id = Model.rootId;
+  // Model static class initialization
+  root[0]["folder_id"] = Model.rootId;
+  Model.workTypes.forEach(str => {
+    workTypes.push(Object.assign({ erp_work_type: str }));
+  });
 });
 
-describe("Unit tests for PostgreSQL", () => {
-  test("should return root id", async () => {
-    return sqlQuery("SELECT folder_id FROM Folders WHERE title = 'Root'").then(
-      response => {
-        expect(response).toEqual(output);
-      }
-    );
+describe("unit tests for PostgreSQL database", () => {
+  test("should return the root id", () => {
+    return expect(
+      sqlQuery("SELECT folder_id FROM Folders WHERE title = 'Root'")
+    ).resolves.toStrictEqual(root);
+  });
+
+  test("should throw a syntax error", () => {
+    expect(
+      sqlQuery("SELECT FROM Folders WHERE title = 'Root'")
+    ).rejects.toThrow();
+  });
+
+  test("should resolve an array of ERP's work types", () => {
+    expect(
+      sqlQuery("SELECT DISTINCT erp_work_type FROM Spaces_info")
+    ).resolves.toEqual(expect.arrayContaining(workTypes));
   });
 });
